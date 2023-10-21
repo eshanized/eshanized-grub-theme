1d6769fdebd6e85bdcf8d44a0fc29be57c9c57a9
ESHANIZED <src.eshan@gmail.com>
2023-10-22 04:23:16 +0530

    ++

--- install-eshanized.py
+++ install-eshanized.py
@@ -12,3 +12,11 @@ R = "\033[0;31m"
 G = "\033[0;32m"  
 B = "\033[0;36m"  
 
+def eshanized_chk_distro():
+    try:
+        lsb_id = subprocess.check_output("lsb_release -i", shell=True).decode("utf-8")
+        id = lsb_id.split(":")[-1].lower().strip()
+    except Exception:
+        id = ""
+    return id
+
